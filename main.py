1808c8ec57daf0dc8a97db1f624fd2613c1598ee
YourUsername <your_email@example.com>
2026-03-19 15:25:51 +0300

    telegram bot is fully working

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="أَهْلًا بِكَ! أَنَا بُوتٌ مَرْفُوعٌ مِنْ تِيرْمُوكْس إِلَى جِيت هَاب.")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="تَمَّ التَّشْغِيلُ بِنَجَاحٍ! هَذَا البُوتُ يَعْمَلُ الآنَ.")
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('TOKEN_HERE').build()
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
+    application = ApplicationBuilder().token('8630383826:AAHEPPRgJuGoAHGgQ6lMQ9K3SiRyciIfjzo').build()
+    application.add_handler(CommandHandler('start', start))
+    print('--- البُوتُ قَيْدُ التَّشْغِيلِ الآنَ ---')
     application.run_polling()
